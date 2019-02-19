fef8077f4f915a56a8c3470c194ffa5b902dd857
Oscar Villarroel <oscar.villarroel@clouddistrict.com>
2019-02-19 18:16:20 +0100

    some changes to go home

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import ResourceList from './ResourceList'
 
 const App = () => {
-  const [resource, setResource] = useState("posts");
+  const [resource, setResource] = useState('posts')
   return (
     <div>
       <div>
-        <button onClick={() => setResource("posts")}>Posts</button>
-        <button onClick={() => setResource("todos")}>Todos</button>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
