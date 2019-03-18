ce9d815a956eaeff71ad67ab12ca9f8360550e33
Oscar Villarroel <oscar.villarroel@clouddistrict.com>
2019-03-18 17:13:35 +0100

    refactored hook so its used as a whole function

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,9 +1,31 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-class ResourceList extends Component {
-  render() {
-    return <div>{this.props.resource}</div>
+const UseResources = resource => {
+  const [resources, setResources] = useState([])
+
+  const fetchResource = async resource => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+    setResources(response.data)
   }
+
+  useEffect(() => {
+    //could also make an IIFE of this
+    fetchResource(resource)
+  }, [resource])
+
+  return resources
+}
+
+const ResourceList = ({ resource }) => {
+  const resources = UseResources(resource)
+  return (
+    <ul>
+      {resources.map(item => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  )
 }
 
 export default ResourceList
